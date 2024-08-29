73928a38190ec137abc7146ed31a7eed88993ca9
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 13:15:39 +0300

    Update weapon level

--- Gun game/WeaponList.lua
+++ Gun game/WeaponList.lua
@@ -1,4 +1,10 @@
 shared.Weaponlist = {
-    "5f1d-02212-fmnz-1q0m-56em-w4gw-6w15-mhob" ; -- m4a1
-    "g288-02212-o17b-qc5o-a73w-64af-dfcf-8e12"; -- glock
+    {
+        name = "M4A1";
+        code =  "5f1d-02212-fmnz-1q0m-56em-w4gw-6w15-mhob"
+    };
+    {
+        name = "Glock17";
+        code =  "g288-02212-o17b-qc5o-a73w-64af-dfcf-8e12"
+    }
 }
--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -1,14 +1,33 @@
 print("gungame, ver:0.32")
 require("Gun%20game/EventsHandler.lua")
+require("Gun%20game/WeaponList.lua")
 --shared.OnPlayerDiedConnection:Disconnect()
 shared.weaponlevels = {}
+
+
+function shared.UpdateWeaponLevel(name)
+    shared.weaponlevels[name] += 1
+
+    local player = get_player(name)
+    local level = shared.weaponlevels[name]
+    local weaponlistData = shared.Weaponlist[level]
+
+    local setup = get_setup_from_code(weaponlistData.code)
+
+    if setup.status ~= "_" then
+        warn("setup is not valid")
+    else
+        -- 1st argument is primary, secondary, throwable1, throwable2
+        player.set_weapon("primary", setup.weapon, setup.data)
+    end
+end
 
 shared.OnPlayerDiedConnection = on_player_died:Connect(function(name, killer_data, stats_counted) 	-- mostly same data the game uses
     print("-----------------")
 	print(name, "died to", killer_data.type, "by", killer_data.name)
 
         local player = get_player(killer_data.name)
-        shared.weaponlevels[killer_data.name] += 1
+        shared.UpdateWeaponLevel(killer_data.name)
      
     -- can be burning, drowning, firearm, grenade, map_reset, other, reset
     print("-----------------")
