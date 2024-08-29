36c714f1e9e8ad1598c8b74713f027bea675acb1
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 12:49:26 +0300

    Update gungame.lua

--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -1,11 +1,11 @@
-print("gungame, ver:0.31")
+print("gungame, ver:0.32")
 require("Gun%20game/EventsHandler.lua")
 --shared.OnPlayerDiedConnection:Disconnect()
 shared.weaponlevels = {}
 
 for i,v in get_players() do
     print(i)
-    weaponlevels[i] = 0
+    shared.weaponlevels[i] = 0
 end
 
 shared.OnPlayerDiedConnection = on_player_died:Connect(function(name, killer_data, stats_counted) 	-- mostly same data the game uses
@@ -13,16 +13,12 @@ shared.OnPlayerDiedConnection = on_player_died:Connect(function(name, killer_dat
 	print(name, "died to", killer_data.type, "by", killer_data.name)
 
         local player = get_player(killer_data.name)
-        player.Weaponlevel += 1
-        print(player.Weaponlevel)
-    
+        shared.weaponlevels[killer_data.name] += 1
+     
     -- can be burning, drowning, firearm, grenade, map_reset, other, reset
 end)
 
 on_player_spawned:Connect(function(name)
     local player = get_player(name)
-    if not player.Weaponlevel then
-        player.Weaponlevel = 0
-    end
-	print(`{name} spawned, gungame level :{player.Weaponlevel}`)
+	print(`{name} spawned, gungame level :{shared.weaponlevels[name]}`)
 end)
