dcfd9075922cdb901157ef5f51e27ddeca5fddb1
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 10:36:42 +0300

    Create EventsHandler.lua

---
+++ Gun game/EventsHandler.lua
@@ -0,0 +1,15 @@
+shared.EventsTable = {
+    kill_player = function(player)
+        player.set_health(math.huge)
+        player.explode()
+    end
+}
+
+
+on_client_event:Connect(function(plrname, args)
+    local player = get_player(plrname)
+    for i,v in player do
+        print(i," ",v)
+    end
+    shared.EventsTable[args[1]](player)
+end)
