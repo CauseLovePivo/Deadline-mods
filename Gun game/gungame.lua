00db9535b4642bec87d43e30720e24be074426fb
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 12:33:21 +0300

    Update gungame.lua

--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -5,11 +5,9 @@ shared.OnPlayerDiedConnection = on_player_died:Connect(function(name, killer_dat
     print("-----------------")
 	print(name, "died to", killer_data.type, "by", killer_data.name)
 
-    if killer_data.name then
         local player = get_player(killer_data.name)
         player.Weaponlevel += 1
         print(player.name, "weapon level is: ", player.Weaponlevel)
-    end
     
     -- can be burning, drowning, firearm, grenade, map_reset, other, reset
 end)
