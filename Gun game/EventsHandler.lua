6a2ae24247dd7ccf616136feaf2e998ef8373823
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 13:08:39 +0300

    Update EventsHandler.lua

--- Gun game/EventsHandler.lua
+++ Gun game/EventsHandler.lua
@@ -1,23 +1,11 @@
 shared.EventsTable = {
-    kill_player = function(player)
+    respawn = function(player)
         player.explode()
     end;
-    update_gungame_stats = function(player)
-        for i,v in get_players() do
-            print(i)
-            shared.weaponlevels[i] = 0
-        end
-    end;
-    get_my_stats = function (player)
+    get_my_stats = function(player)
         for i,v in player do
-            if not type(v) == table then
-                print(i," ",v)
-            else
-                for j,v2 in v do
-                    print(j2," ",v2)
-                end
-            end      
-        end        
+            print(i," ",v)    
+        end
     end
 }
 
