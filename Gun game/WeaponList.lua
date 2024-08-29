9ec6fd8095cd549f469dda19d063d4a9a37accd3
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 13:47:34 +0300

    Update WeaponList.lua

--- Gun game/WeaponList.lua
+++ Gun game/WeaponList.lua
@@ -6,5 +6,17 @@ shared.Weaponlist = {
     {
         name = "Glock17";
         code =  "g288-02212-o17b-qc5o-a73w-64af-dfcf-8e12"
-    }
+    };
+    {
+        name = "Vector";
+        code = "7oee-02212-dgf6-eaz5-n271-n8d4-9e7w-z51n"
+    };
+    {
+        name = "AUG_A3";
+        code = "0qcc-02212-f99q-6868-6972-d8d5-q52z-ww36"
+    };
+    {
+        name = "Makarov";
+        code = "3bb7-02212-nec2-zoab-zhha-697n-g2o3-2dd5"
+    };
 }
