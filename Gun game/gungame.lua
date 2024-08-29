ff15ad4d2d70f0db1a34a4ddc2f54591fc0fa2fd
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 11:11:58 +0300

    Update gungame.lua

--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -1,6 +1,6 @@
 require("Gun%20game/EventsHandler.lua")
 
-on_player_died:Connect(function(name, killer_data, stats_counted)
+shared.OnPlayerDiedConnection = on_player_died:Connect(function(name, killer_data, stats_counted)
 	-- mostly same data the game uses
     print("-----------------")
 	print(name, "died to", killer_data.typ, "by", killer_data.name) -- can be burning, drowning, firearm, grenade, map_reset, other, reset
