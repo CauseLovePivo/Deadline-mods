b3b1c08c699114fd203ba63fcdd576300f91afbd
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 10:54:27 +0300

    Update gungame.lua

--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -4,5 +4,14 @@ on_player_died:Connect(function(name, killer_data, stats_counted)
 	-- mostly same data the game uses
 
 	print(name, "died to", killer_data.type) -- can be burning, drowning, firearm, grenade, map_reset, other, reset
-    print(killer_data)
+    for i,v in killer_data do
+        print(i,":",v)
+        if type(v) == "table" then
+            print("-----------------")
+            for j,v2 in v do
+                print(j,":",v2)
+            end
+            print("-----------------")
+        end
+    end
 end)
