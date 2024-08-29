38fa8ba87bdb1314b8f03fe61f923577c16b9dc8
CauseLovePivo <svyatikstanko@gmail.com>
2024-08-29 10:44:12 +0300

    on player died event added

--- Gun game/gungame.lua
+++ Gun game/gungame.lua
@@ -1,2 +1,8 @@
-print("Gungame initialized!")
 require("Gun%20game/EventsHandler.lua")
+
+on_player_died:Connect(function(name, killer_data, stats_counted)
+	-- mostly same data the game uses
+
+	print(name, "died to", killer_data.type) -- can be burning, drowning, firearm, grenade, map_reset, other, reset
+    print(killer_data)
+end)
